78a4d01dd9a49d44f92d4af49f0e9426ca55a7cc
kim hanwoong <khw7096@gmail.com>
2018-09-16 00:13:00 +0900

    내용추가함.

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
 awless create s3object bucket=lazypic.org file=./index.html
 awless create s3object bucket=lazypic.org file=./storyboard.html
+awless create s3object bucket=lazypic.org file=./circle.html
