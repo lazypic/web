2eadb2cd09f19f36d4b9f3d06035df00a337c69d
kim hanwoong <khw7096@gmail.com>
2018-09-25 14:47:05 +0900

    add histroy page

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 awless create s3object bucket=lazypic.org file=./index.html
 awless create s3object bucket=lazypic.org file=./storyboard.html
 awless create s3object bucket=lazypic.org file=./circle.html
+awless create s3object bucket=lazypic.org file=./projecthistory.html
