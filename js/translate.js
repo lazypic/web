3de9195dc06bcb18d42e72af6f70257202e53b50
hanwoong kim <hello@lazypic.org>
2019-09-07 00:43:24 +0900

    버튼이 작동되도록 바꿈.

---
+++ js/translate.js
@@ -0,0 +1,44 @@
+var korean = document.getElementById('kor_click'),
+    english = document.getElementById('eng_click'),
+    korean_txt = document.querySelectorAll('.kor'),
+    english_txt = document.querySelectorAll('.eng'),
+    nb_korean = korean_txt.length,
+    nb_english = english_txt.length;
+
+korean.addEventListener('click', function() {
+    langue(korean,english);
+}, false);
+
+english.addEventListener('click', function() {
+    langue(english,korean);
+}, false);
+
+function langue(langueOn,langueOff){
+    if (!langueOn.classList.contains('current_lang')) {
+        langueOn.classList.toggle('current_lang');
+        langueOff.classList.toggle('current_lang');
+    }
+    if(langueOn.innerHTML == '한글'){
+        afficher(korean_txt, nb_korean);
+        cacher(english_txt, nb_english);
+    }
+    else if(langueOn.innerHTML == 'English'){
+        afficher(english_txt, nb_english);
+        cacher(korean_txt, nb_korean);
+    }
+}
+
+function afficher(txt,nb){
+    for(var i=0; i < nb; i++){
+        txt[i].style.display = 'block';
+    }
+}
+function cacher(txt,nb){
+    for(var i=0; i < nb; i++){
+        txt[i].style.display = 'none';
+    }
+}
+function init(){
+    langue(english,korean);
+}
+init();
